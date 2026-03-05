23390bde27607f9fbe7b59c12353741e9765730d
Levi Sandidge <ljsandidge@gmail.com>
2026-03-05 15:59:27 -0500

    started implementing SDL3 and added call and ret instruction

--- chip-8.c
+++ chip-8.c
@@ -11,7 +11,11 @@
 
 #include <stdlib.h>
 
-#define MAX_SIZE 12
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#define PROGRAM_START 0x200
+#define MAX_SIZE 16
 #define RAM_SIZE 4096
 
 typedef uint8_t BYTE;
@@ -19,40 +23,84 @@ typedef int8_t SIGNED_BYTE;
 typedef uint16_t WORD;
 typedef int16_t SIGNED_WORD;
 
-/*
-Font:
-0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-0x20, 0x60, 0x20, 0x20, 0x70, // 1
-0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-0xF0, 0x80, 0xF0, 0x80, 0x80  // F
-*/
+// TODO: Setup SDL3
 
-typedef struct 
-{
-    BYTE arr[MAX_SIZE];
+typedef struct {
+    SDL_Window* win;
+    SDL_Renderer* ren;
+} SDL_GRAPHICS;
+
+void initilize_window(SDL_GRAPHICS* window) {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    window->win = SDL_CreateWindow("CHIP-8", 640, 320, SDL_WINDOW_OPENGL | SDL_WINDOW_ALWAYS_ON_TOP);
+
+    window->ren = SDL_CreateRenderer(window->win, NULL);
+
+    SDL_SetRenderDrawColor(window->ren, 0, 0, 0, 255);
+    SDL_RenderClear(window->ren);
+    SDL_RenderPresent(window->ren); // Show the black screen
+
+    bool done = false;
+
+    while (!done) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
+            }
+        }
+
+        // Do game logic, present a frame, etc.
+    }
+
+}
+
+void render(SDL_GRAPHICS* window) {
+
+
+    SDL_RenderPresent(window->ren);
+}
+
+void delete_window(SDL_GRAPHICS* window) {
+    SDL_DestroyRenderer(window->ren);
+
+    SDL_DestroyWindow(window->win);
+
+    SDL_Quit();
+}
+
+BYTE font[] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+typedef struct {
+    WORD arr[MAX_SIZE];
 } STACK;
 
-typedef struct 
-{
+typedef struct {
     bool arr[64][32];
 } SCREEN;
 
-typedef struct
-{
+typedef struct {
     // 2 byte instructions
-    WORD  instruction;
+    WORD instruction;
 
     WORD PC, SP, I;
 
@@ -64,16 +112,26 @@ typedef struct
     BYTE V[16];
 } CPU;
 
-void DXYN(CPU *cpu, SCREEN *screen, WORD *RAM, BYTE first, BYTE second) {
-    WORD x = cpu->V[first & 0xF] % 64;
-    WORD y = cpu->V[(second & 0xF0) >> 4] % 32;
-    BYTE n = second & 0xF;
+void call(STACK* stack, CPU* cpu, WORD NNN) {
+    stack->arr[cpu->SP] = cpu->PC;
+    cpu->SP++;
+    cpu->PC = NNN;
+}
+
+WORD ret(STACK* stack, CPU* cpu) {
+    cpu->SP--;
+    return stack->arr[cpu->SP];
+}
+
+void DXYN(CPU *cpu, SCREEN *screen, BYTE *RAM, BYTE X, BYTE Y, BYTE N) {
+    WORD x = cpu->V[X] % 64;
+    WORD y = cpu->V[Y] % 32;
     BYTE sprite;
 
     cpu->V[0xF] = 0;
 
-    for (int i = 0; i < n; i++) {
-        if (y + i> 31) {
+    for (int i = 0; i < N; i++) {
+        if (y + i > 31) {
             continue;
         }
 
@@ -82,15 +140,13 @@ void DXYN(CPU *cpu, SCREEN *screen, WORD *RAM, BYTE first, BYTE second) {
             if (x + j > 63) {
                 continue;
             }
-            screen->arr[x+j][y+i] = screen->arr[x+j][y+i] ^ ((sprite >> j) & 0x0001);
+            screen->arr[x+j][y+i] = screen->arr[x + j][y + i] ^ ((sprite >> j) & 0x0001);
             if (screen->arr[x+i][y+j] == true && (sprite >> j) == 1) {
                 cpu->V[0xF] = 1;
             }
         }
     }
 }
-
-// TODO: Setup SDL3
 
 int main(int argc, char* argv[]) {
     // Add a log file for errors
@@ -124,71 +180,86 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // TODO: Fix by malloc 4k of memory and memcopy rom into memory
+    // TODO: Fix by calloc 4k of memory and memcopy rom into memory
     // TODO: Map font into ram as well
-    WORD *RAM = malloc(RAM_SIZE * sizeof(BYTE));
+    //BYTE *RAM = calloc(RAM_SIZE, sizeof(BYTE));
+    BYTE RAM[RAM_SIZE] = {0};
     WORD *rom = mmap(NULL, rom_info.st_size, PROT_READ, MAP_PRIVATE, rom_fd, 0);
     if (rom == MAP_FAILED) {
         perror("MMAP Failed");
+        close(rom_fd);
         return 1;
     }
-    memcpy(RAM + 0x200, rom,rom_info.st_size);
+    close(rom_fd);
+    memcpy(RAM, &font, sizeof(font));
+    memcpy(RAM + PROGRAM_START, rom,rom_info.st_size);
+    munmap(rom, rom_info.st_size);
 
     //TODO: setup cpu
     CPU cpu;
-    cpu.PC = 0x200;
+    cpu.PC = PROGRAM_START;
 
     SCREEN screen = {0};
 
     STACK stack = {0};
     cpu.SP = 0;
 
-    // TODO: fetch:
-    // Get each nibble out of the instruction to be able to decode instruction
+    SDL_GRAPHICS window;
 
-    for (int i = 0; i < 40; i++) {
-        cpu.instruction = RAM[cpu.PC];
-        cpu.instruction = __builtin_bswap16(cpu.instruction);
+   initilize_window(&window);
+
+    for (int i = 0; i < 42; i++) {
+         // TODO: fetch:
+        // Get each nibble out of the instruction to be able to decode instruction
+        cpu.instruction = RAM[cpu.PC] << 8 | RAM[cpu.PC + 1];
+        //cpu.instruction = __builtin_bswap16(cpu.instruction);
         //printf("%#04X\n", cpu.instruction);
-        BYTE first = (cpu.instruction & 0xFF00) >> 4;
-        BYTE second = cpu.instruction & 0x00FF;
 
-        cpu.PC += 1;
+        BYTE N1 = (cpu.instruction & 0xF000) >> 12;
+        BYTE X = (cpu.instruction & 0x0F00) >> 8;
+        BYTE Y = (cpu.instruction & 0x00F0) >> 4;
+        BYTE N = cpu.instruction & 0x000F;
+        
+        BYTE NN = cpu.instruction & 0x00FF;
+        WORD NNN = cpu.instruction & 0x0FFF;
+
+        printf("%X\n", cpu.instruction);
+
+        cpu.PC += 2;
 
         // TODO: decode and execute within the switch statement
-        switch(first & 0xF0) {
-            case 0x10:
-                cpu.PC = ((WORD)(first & 0x0F) << 8) + second;
+        switch(N1) {
+            case 0x0:
+                if (NNN == 0x0E0) {
+                    memset(screen.arr, 0, sizeof(screen.arr));
+                }
                 break;
-            case 0x60:
-                cpu.V[first & 0x0F] = second;
+            case 0x1:
+                cpu.PC = NNN;
                 break;
-            case 0x70:
-                cpu.V[first & 0x0F] += second;
+            case 0x6:
+                cpu.V[X] = NN;
                 break;
-            case 0xA0:
-                cpu.I = ((WORD)(first & 0x0F) << 8) + second;
+            case 0x7:
+                cpu.V[X] += NN;
                 break;
-            case 0xD0:
-                DXYN(&cpu, &screen, RAM, first, second);
+            case 0xA:
+                cpu.I = NNN;
+                break;
+            case 0xD:
+                DXYN(&cpu, &screen, RAM, X, Y, N);
                 break;
         }
-
-        switch (cpu.instruction) {
-            case 0x00E0: 
-                memset(screen.arr, 0, sizeof(*screen.arr));
-                break;
-        };
     }
-
-
-    /*
+    
     for (int i = 0; i < 32; i++) {
         for (int j = 0; j < 64; j++) {
             printf("%d ", screen.arr[j][i]);
         }
         printf("\n");
     }
-    */
+
+    delete_window(&window);
+
     return 0;
 }
